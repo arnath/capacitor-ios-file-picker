47b840c584754e7703b60d12e549ede1fec1aab9
Vijay Prakash <prakash.vijay@gmail.com>
2023-01-12 11:03:25 +0100

    Initial commit

---
+++ android/src/main/java/dev/vijayp/capacitoriosfilepicker/FilePicker.java
@@ -0,0 +1,11 @@
+package dev.vijayp.capacitoriosfilepicker;
+
+import android.util.Log;
+
+public class FilePicker {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
